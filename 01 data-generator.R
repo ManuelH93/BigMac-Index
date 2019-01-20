4fd81816ce79caf46040ef271598139440f79ffb
Manuel <manuelheller@outlook.com>
2019-01-20 13:42:56 +0100

    Complete adjusted index

--- 01 data-generator.R
+++ 01 data-generator.R
@@ -126,6 +126,11 @@ ggplot(to_plot[, over := USD > 0], aes(x=name, y=USD, color=over)) +
 #6 Adjusted index
 #######################################################################
 
+#Part of the difference we observe in Burger prices may not be due to
+#the currency being overvalued/undervalued, but due to the difference in non-traded
+#local input prices (wage and rent). GDP is used as a proxy for those non-traded
+#local input prices.
+
 #Only keep observations where we have GDP data
 
 big_mac_gdp_data = big_mac_data[GDP_dollar > 0]
@@ -150,9 +155,67 @@ rm(test, test2)
 #why we aren't using all.
 #######################################################################
 
+#Create dataset that only has countries in it which we want to use for the
+#regression
+
 big_mac_gdp_data = big_mac_gdp_data[iso_a3 %in% regression_countries]
+
+#Plot GDP against prices as well as linear regression line
 
 ggplot(big_mac_gdp_data, aes(x=GDP_dollar, y=dollar_price)) +
   facet_wrap(~date) +
   geom_smooth(method = lm, color='tomato') +
   geom_point(alpha=0.5)
+
+#run regression and add predicion to dataset
+big_mac_gdp_data[,adj_price := lm(dollar_price ~ GDP_dollar) %>% predict,by=date]
+
+#Check if predicted values were added to dataset
+tail(big_mac_gdp_data)
+
+#Chek to make sure regression did what we intended it to do. Predicted points
+#should all lie on line of best fit.
+ggplot(big_mac_gdp_data, aes(x=GDP_dollar, y=dollar_price)) +
+  facet_wrap(~date) +
+  geom_smooth(method = lm, color='tomato') +
+  geom_linerange(aes(ymin=dollar_price, ymax=adj_price), color='royalblue', alpha=0.3) +
+  geom_point(alpha=0.1) +
+  geom_point(aes(y=adj_price), color='royalblue', alpha=0.5)
+
+#Keep variables and countries used for creation of adjusted index
+big_mac_adj_index = big_mac_gdp_data[
+  iso_a3 %in% regression_countries & iso_a3 %in% big_mac_countries
+  ,.(date, iso_a3, currency_code, name, local_price, dollar_ex, dollar_price, GDP_dollar, adj_price)]
+
+#Create adjusted index for each currency
+for(currency in base_currencies) {
+  big_mac_adj_index[
+    ,                           
+    (currency) := (dollar_price / adj_price) / (.SD[currency_code == currency]$dollar_price / .SD[currency_code == currency]$adj_price)- 1,
+    by=date
+    ]
+}
+
+#Round index to three decimal points
+big_mac_adj_index[, (base_currencies) := round(.SD, 3), .SDcols=base_currencies]
+
+#Plot adjusted index
+to_plot = big_mac_adj_index[date == latest_date]
+to_plot$name = factor(to_plot$name, levels=to_plot$name[order(to_plot$USD)])
+ggplot(to_plot[, over := USD > 0], aes(x=name, y=USD, color=over)) +
+  geom_hline(yintercept = 0) +
+  geom_linerange(aes(ymin=0, ymax=USD)) +
+  geom_point() +
+  coord_flip()
+
+#Export adjusted index
+fwrite(big_mac_adj_index, '/Users/manuelheller/Documents/Data Science/Economist Github/Outputs/big-mac-adj-index.csv')
+
+#Merge both indexes
+big_mac_full_index = merge(big_mac_index, big_mac_adj_index,
+                           by=c('date', 'iso_a3', 'currency_code', 'name', 'local_price', 'dollar_ex', 'dollar_price'),
+                           suffixes=c('_raw', '_adjusted'),
+                           all.x=TRUE
+)
+
+fwrite(big_mac_full_index, '/Users/manuelheller/Documents/Data Science/Economist Github/Outputs/big-mac-full-index.csv')
